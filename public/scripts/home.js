f0516852d8d8cb5c6bd69c372568e2150a6c6154
helenohyeah <ouyang.helen@gmail.com>
2020-10-17 19:04:25 +0000

    Add logout route that clears cookies

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,8 +1,15 @@
 $(() => {
-  $('#login').on('click', () => {
+  $("#login").on("click", () => {
     $.ajax({
       method: "POST",
       url: "/login"
     })
   });
+
+  $("#logout").on("click", () => {
+    $.ajax({
+      method: "POST",
+      url: "/logout"
+    })
+  })
 });
