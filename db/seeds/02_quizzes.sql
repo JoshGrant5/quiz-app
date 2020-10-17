afa1f1b0e7741ac4567400a16d80f21cdbec5cd0
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-16 21:56:51 -0600

    Created functions to get quiz, questions and answers

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,12 +1,12 @@
 INSERT INTO quizzes (creator_id, title, photo, listed, url, category, date_created)
-VALUES (1, 'Happy Quiz', 'https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/', true, '/testurl1', 'History', '2020-10-16');
+VALUES (1, 'Happy Quiz', 'https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/', true, 'testurl1', 'History', '2020-10-16');
 
 INSERT INTO quizzes (creator_id, title, photo, listed, url, category, date_created)
-VALUES (2, 'Sad Quiz', 'https://www.pexels.com/photo/raw-almonds-spilled-out-of-small-ceramic-bowl-on-table-4033325/', true, '/testurl2', 'Random', '2020-10-16');
+VALUES (2, 'Sad Quiz', 'https://www.pexels.com/photo/raw-almonds-spilled-out-of-small-ceramic-bowl-on-table-4033325/', true, 'testurl2', 'Random', '2020-10-16');
 
 INSERT INTO quizzes (creator_id, title, photo, listed, url, category, date_created)
-VALUES (1, 'Bleh Quiz', 'https://www.pexels.com/photo/photo-of-paint-splatter-artwork-1000366/', true, '/testurl3', 'Random', '2020-10-16');
+VALUES (1, 'Bleh Quiz', 'https://www.pexels.com/photo/photo-of-paint-splatter-artwork-1000366/', true, 'testurl3', 'Random', '2020-10-16');
 
 INSERT INTO quizzes (creator_id, title, photo, listed, url, category, date_created)
-VALUES (2, 'Mehhhhh Quiz', 'https://www.pexels.com/photo/official-currency-united-states-of-america-4386404/', true, '/testurl3', 'History', '2020-10-16');
+VALUES (2, 'Mehhhhh Quiz', 'https://www.pexels.com/photo/official-currency-united-states-of-america-4386404/', true, 'testurl4', 'History', '2020-10-16');
 
