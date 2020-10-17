ea42f4771df737b63dc854e921f5fda895660d8c
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-17 11:18:58 -0600

    Added getting all answers through promise.all and posting quiz attempt

--- routes/quiz.js
+++ routes/quiz.js
@@ -13,6 +13,21 @@ module.exports = (helpers) => {
     helpers.getAllQuizzes().then(info => res.json(info));
   });
 
+  router.get('/create', (req, res) => {
+    res.render('create_quiz');
+  });
+
+  router.post('/create', (req, res) => {
+    helpers.createNewQuiz(req.body);
+
+    // Obtain quiz ID from newly created quiz
+
+    // helpers.createQuestion()
+    // helpers.createAnswer()
+    console.log(req.body);
+    res.redirect('index');
+  });
+
   router.get("/:url", (req, res) => {
     let quizInfo = {}
     helpers.getQuizWithUrl(req.params.url)
@@ -22,12 +37,31 @@ module.exports = (helpers) => {
       })
       .then(questions => {
         quizInfo.questions = questions;
-        return helpers.getAnswersForQuiz(quizInfo.quiz.id);
+        const answers = [];
+        questions.forEach(question => answers.push(helpers.getAnswers(question.id)));
+        return Promise.all(answers);
       })
-      .then(answers => {
-        quizInfo.answers = answers;
-        res.json(quizInfo);
+      .then((answers) => {
+        for (let i = 0; i < answers.length; i++) {
+          quizInfo.questions[i].answers = answers[i];
+        }
+        res.render('take_quiz', quizInfo);
+        // res.json(quizInfo);
       });
+  });
+
+  router.post("/:url", (req, res) => {
+    let score = 0;
+    helpers.getScore(req.body)
+      .then(answers => {
+        score = answers.score;
+        return helpers.getQuizWithUrl(req.params.url);
+      })
+      .then(quiz => {
+        return helpers.createResult(quiz.id, score, 1); //1 is user_id, hard-coded currently
+      })
+      .then(result => res.json(result));
+      // .then(result => res.redirect(`/quiz/${req.params.url}/result/${result.id}`));
   });
 
   return router;
