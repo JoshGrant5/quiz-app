3ed101b126a03048d552c496f4d0d50ad02bbc09
helenohyeah <ouyang.helen@gmail.com>
2020-10-17 16:00:09 +0000

    Create dummy homepage to ctest all routes

--- routes/home.js
+++ routes/home.js
@@ -10,7 +10,7 @@ const router  = express.Router();
 
 module.exports = ({ helpers }) => {
   router.get("/", (req, res) => {
-    res.send('home');
+    res.render('index');
   });
 
   return router;
