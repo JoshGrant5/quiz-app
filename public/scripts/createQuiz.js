5692da545bbc32bb8f9466b31f43107b556aec53
JoshGrant5 <joshgg@icloud.com>
2020-10-18 23:53:34 +0000

    Changed radio buttons on answers to checkboxes

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -1,4 +1,4 @@
-$(() => {
+$(document).ready(function() {
   // Question count: Starts at 1 since we already have 1 question in viewport at start
   let count = 1;
 
@@ -6,24 +6,45 @@ $(() => {
     count++;
     return `
     <div class='newQuestion'>
-      <div>
-        <h2>Question ${count}:</h2>
-        <button type='button' class='btn btn-outline-danger' id='deleteQuestion${count}' >X</button>
-      </div>
-      <input type='text' name='question${count}' required='required'>
-      <label>A)</label>
-      <input type='text' name='a${count}' required='required'>
-      <input type='radio' name='correct${count}' required='required' value='1'>
-      <label>B)</label>
-      <input type='text' name='b${count}' required='required'>
-      <input type='radio' name='correct${count}' value='2'>
-      <label>C)</label>
-      <input type='text' name='c${count}' required='required'>
-      <input type='radio' name='correct${count}' value='3'>
-       <label>D)</label>
-      <input type='text' name='d${count}' required='required'>
-      <input type='radio' name='correct${count}' value='4'>
+    <div>
+      <h2>Question ${count}:</h2>
+      <button type="button" class="deleteQuestion btn btn-outline-danger" id='deleteQuestion${count}'>X</button>
     </div>
+    <div class='question'>
+      <input type='text' name='question${count}' required='required'>
+    </div>
+    <h3>Answers:</h3>
+    <div class='answers' data-toggle="buttons">
+      <div class='singleLine'>
+        <label>A)</label>
+        <input type='text' name='a${count}' required='required'>
+        <label class='btn btn-outline-success'>
+          <input type='radio' name='correct${count}' autocomplete='off' required='required' value='1'><i class="gg-check"></i>
+        </label>
+      </div>
+      <div class='singleLine'>
+        <label>B)</label>
+        <input type='text' name='b${count}' required='required'>
+        <label class='btn btn-outline-success'>
+          <input type='radio' name='correct${count}' value='2'><i class="gg-check"></i>
+        </label>
+      </div>
+      <div class='singleLine'>
+        <label>C)</label>
+        <input type='text' name='c${count}' required='required'>
+        <label class='btn btn-outline-success'>
+          <input type='radio' name='correct${count}' value='3'><i class="gg-check"></i>
+        </label>
+      </div>
+      <div class='singleLine'>
+        <label>D)</label>
+        <input type='text' name='d${count}' required='required'>
+        <label class='btn btn-outline-success'>
+          <input type='radio' name='correct${count}' value='4'><i class="gg-check"></i>
+        </label>
+      </div>
+    </div>
+  </div>
   `
   };
 
