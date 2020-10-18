ed12ace3a82700cf1fa2171c62fb008623a821df
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-18 14:47:07 -0600

    Altered results seeds to match new quizzes

--- db/seeds/05_results.sql
+++ db/seeds/05_results.sql
@@ -1,35 +1,35 @@
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (1, 1, 1, 2, '2020-10-16');
+VALUES (1, 1, 4, 5, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (1, 2, 2, 2, '2020-10-16');
+VALUES (1, 2, 3, 5, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (1, 3, 1, 2, '2020-10-16');
+VALUES (1, 3, 2, 5, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (1, 4, 2, 2, '2020-10-16');
+VALUES (1, 4, 5, 5, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (2, 1, 1, 2, '2020-10-16');
+VALUES (2, 1, 4, 6, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (3, 2, 2, 2, '2020-10-16');
+VALUES (3, 2, 3, 3, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (4, 3, 1, 2, '2020-10-16');
+VALUES (4, 3, 2, 4, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (2, 4, 2, 2, '2020-10-16');
+VALUES (2, 4, 1, 6, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (2, 3, 1, 2, '2020-10-16');
+VALUES (2, 3, 5, 6, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (3, 1, 2, 2, '2020-10-16');
+VALUES (3, 1, 2, 3, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (4, 4, 1, 2, '2020-10-16');
+VALUES (4, 4, 1, 4, '2020-10-16');
 
 INSERT INTO results (quiz_id, user_id, score, total, date_completed)
-VALUES (2, 2, 2, 2, '2020-10-16');
+VALUES (2, 2, 2, 6, '2020-10-16');
