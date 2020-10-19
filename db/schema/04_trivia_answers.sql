8e2030b7e90dde4ef87496c725efcb97fd45aba7
JoshGrant5 <joshgg@icloud.com>
2020-10-19 17:26:10 +0000

    trivia_answers references trivia_questions

--- db/schema/04_trivia_answers.sql
+++ db/schema/04_trivia_answers.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS trivia_answers CASCADE;
 CREATE TABLE trivia_answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  question_id INTEGER REFERENCES trivia_questions(id) ON DELETE CASCADE,
   answer TEXT NOT NULL,
   is_correct BOOLEAN DEFAULT FALSE
 );
