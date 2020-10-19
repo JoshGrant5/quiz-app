03192a91bee10dd34df4c72e24e1d521a7658cbc
JoshGrant5 <joshgg@icloud.com>
2020-10-19 17:49:04 +0000

    Changed order of personality results and answers

--- db/schema/09_personality_answers.sql
+++ db/schema/09_personality_answers.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS personality_answers CASCADE;
 
 CREATE TABLE personality_answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  questions_id INTEGER REFERENCES personality_questions(id) ON DELETE CASCADE,
-  result_id INTEGER REFERENCES personality_results(id) ON DELETE CASCADE,
+  question_id INTEGER REFERENCES personality_questions(id) ON DELETE CASCADE,
+  outcome_id INTEGER REFERENCES personality_outcomes(id) ON DELETE CASCADE,
   answer TEXT NOT NULL,
   photo TEXT
 );
