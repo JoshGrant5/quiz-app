305d7531ffda92ec9fdbb386683e077c58c90a20
JoshGrant5 <joshgg@icloud.com>
2020-10-19 17:22:36 +0000

    Renamed questions table to trivia_questions

--- db/schema/03_trivia_questions.sql
+++ db/schema/03_trivia_questions.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS questions CASCADE;
 
-CREATE TABLE questions (
+CREATE TABLE trivia_questions (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   question TEXT NOT NULL
