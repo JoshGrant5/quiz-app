ec89364f8832f1c2e93d0073e36a52039e3d0ff8
helenohyeah <ouyang.helen@gmail.com>
2020-10-20 18:13:49 +0000

    Update click handler to sorting and filtering

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,57 @@
 $(() => {
 
   const $quizContainer = $('#quiz-container');
+  const $sortAndFilterBtns = $(".quiz-view-options button");
+  const $filterBtns = $(".quiz-filter button");
+  const $sortBtns = $(".quiz-sort button");
+  
+  // click handler on quiz filtering and sorting
+  $sortAndFilterBtns.on("click", function(e) {
+
+    const selectionType = $(this).parent().attr("class");
+    let filterName, filterType, sortBy;
+    
+    // if filter selected, filter = target, sort = styled
+    if (selectionType === "quiz-filter") {
+      filterName = $(this).text();
+      filterType = $(this).attr("name");
+      sortBy = $(".quiz-sort button.btn-primary").attr("name");
+    
+    // if sort selected, sort = target, filter = styled
+    } else {
+      // filterName = $(".quiz-filter button.btn-primary").text();
+      filterName = $(".quiz-filter button.btn-primary").text();
+      filterType = $(".quiz-filter button.btn-primary").attr("name");
+      sortBy = $(this).attr("name");
+    }
+
+    // send sort and filter and get an array of quizzes
+    $.ajax({
+      method: "GET",
+      url: "/api/filterAndSort",
+      data: { filterType, filterName, sortBy }
+    }).then((res) => {
+      // empty quiz container and show filtered quizzes in sort order
+      $quizContainer.empty();
+      renderQuizzes(res);
+
+      // toggle button styles so only active filter/sort is solid
+      toggleBtns(selectionType);
+      $(e.target).removeClass("btn-outline-primary").addClass("btn-primary");
+    }).catch((err) => err.message);
+  });
+
+  // toggle style of filter and sort button given selected button
+  const toggleBtns = (selection) => {
+    let buttons;
+    (selection === 'quiz-filter') ? buttons = $filterBtns : buttons = $sortBtns;
+    
+    buttons.each(function() {
+      if($(this).hasClass("btn-primary")) {
+        $(this).removeClass("btn-primary").addClass("btn-outline-primary");
+      }
+    });
+  };
 
   // creates a quiz article element
   const createQuizElement = (quiz) => {
@@ -27,54 +78,4 @@ $(() => {
       $quizContainer.append($quiz);
     })
   };
-
-  const $sortAndFilterBtns = $(".quiz-view-options button");
-  
-  // click handler on quiz category filter
-  $sortAndFilterBtns.on("click", function(e) {
-    const $filterBtns = $(".quiz-filter button");
-    // const $sortBtns = $(".quiz-sort button");
-    // console.log('filters>>', $filterBtns);
-    // console.log('sorts>>', $sortBtns);
-
-    const selectionType = $(this).parent().attr("class");
-    let filterName, filterType, sortBy;
-    // if filter selected, filter = target, sort = styled
-    if (selectionType === "quiz-filter") {
-      filterName = $(this).text();
-      filterType = $(this).attr("name");
-      sortBy = $(".quiz-sort button.btn-primary").attr("name");
-    
-    // if sort selected, sort = target, filter = styled
-    } else {
-      filterName = $(".quiz-filter button.btn-primary").text();
-      filterType = $(".quiz-filter button.btn-primary").attr("name");
-      sortBy = $(this).attr("name");
-    }
-
-    // console.log('filterName>>', filterName);
-    // console.log('filterType>>', filterType);
-    // console.log('sortby>>', sortBy);
-
-    // gets category button text
-    // const filterType = $(this).attr("name");
-    // const filterName = $(this).text();
-    
-    // renders new quiz view based on filter and sort options
-    $.ajax({
-      method: "GET",
-      url: "/api/filterAndSort",
-      data: { filterType, filterName, sortBy }
-    }).then((res) => {
-      // empty quiz container and show filtered ones
-      $quizContainer.empty();
-      renderQuizzes(res);
-
-      // change selected filter to solid and unselected filter to outline
-      $filterBtns.each(function() {
-        if($(this).hasClass("btn-primary")) $(this).removeClass("btn-primary").addClass("btn-outline-primary");
-      });
-      $(e.target).removeClass("btn-outline-primary").addClass("btn-primary");
-    }).catch((err) => err.message);
-  });
 });
