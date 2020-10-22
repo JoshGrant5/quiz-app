4a95e7988517583387630e41ee6cd912726408b6
helenohyeah <ouyang.helen@gmail.com>
2020-10-22 19:38:19 +0000

    Remove unused code in home script

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,9 +1,5 @@
 $(() => {
 
-  const $quizContainer = $('#quiz-container');
-  const $sortAndFilterBtns = $(".quiz-view-options button");
-  const $filterBtns = $(".quiz-filter button");
-  const $sortBtns = $(".quiz-sort button");
   let filterName = "All";
   let filterType = undefined;
   let sortName = "created";
@@ -27,6 +23,9 @@ $(() => {
   // run on page load
   loadQuizzes();
 
+  const $sortAndFilterBtns = $(".quiz-view-options button");
+  const $filterBtns = $(".quiz-filter button");
+  const $sortBtns = $(".quiz-sort button");
   // click handler on quiz filtering and sorting
   $sortAndFilterBtns.on("click", function(e) {
     count = 0;
@@ -49,7 +48,7 @@ $(() => {
     // send sort and filter options, returns an array of quizzes
     $.ajax({
       method: "GET",
-      url: "/api/quizzes",
+      url: "/api/partial/view_quizzes",
       data: { filterType, filterName, sortName, sortOrder }
     }).then((res) => {
       // toggle button styles so only active filter/sort is solid
@@ -57,8 +56,9 @@ $(() => {
       $(e.target).removeClass("btn-outline-primary").addClass("btn-primary");
       
       // empty quiz container and show filtered quizzes in sort order
-      $quizContainer.empty();
-      renderQuizzes(res);
+      $("#container").empty();
+      $("#container").append(res);
+      checkScroll();
     }).catch(err => err.message);
   });
 
@@ -72,54 +72,6 @@ $(() => {
         $(this).removeClass("btn-primary").addClass("btn-outline-primary");
       }
     });
-  };
-
-  // creates a quiz article element
-  const createQuizElement = (quiz) => {
-    // set a filler photo and description if null
-    if (!quiz.photo) {
-      quiz.photo = '/imgs/temp-photo.jpg';
-    }
-    if (!quiz.description) {
-      quiz.description = '';
-    }
-
-    const titleChar = quiz.title.length;
-    const descChar = quiz.description.length;
-    // truncate title if it's longer than 2 lines
-    if (titleChar >= 40) {
-      quiz.title = truncate(quiz.title, 40);
-    }
-    // truncate description depending on whether title takes up 1 or 2 lines
-    if (titleChar >= 25 && descChar >= 50) {
-      quiz.description = truncate(quiz.description, 50);
-    } else if (quiz.description.length >= 80) {
-      quiz.description = truncate(quiz.description, 80);
-    }
-
-    let $quiz = $(`
-    <a href="/quiz/${quiz.url}">
-      <article class="card quiz">
-        <img class="card-img-top" src="${quiz.photo}">
-        <div class="card-body">
-          <h3 class="card-title">${quiz.title}</h3>
-          <p class="card-subtitle">${quiz.type} | ${quiz.category}</p>
-          <p class="card-text">${quiz.description}</p>
-        </div>
-      </article>
-    </a>
-    `);
-
-    return $quiz;
-  };
-
-  // renders all quizzes into the quiz container
-  const renderQuizzes = (quizzes) => {
-    quizzes.forEach((quiz) => {
-      const $quiz = createQuizElement(quiz);
-      $quizContainer.append($quiz);
-    })
-    checkScroll();
   };
 
   const checkScroll = function() {
@@ -147,10 +99,4 @@ $(() => {
   const buffer = 50;
 
   $(window).scroll(checkScroll);
-
-  // truncate string given the string and character limit
-  const truncate = (string, limit) => {
-    const shorten = string.slice(0, limit);
-    return shorten.concat("...");
-  }
 });
