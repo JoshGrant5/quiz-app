41be065e8e421d16471b87bb84b77bb7d029c415
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-22 17:54:02 -0600

    Deleted questions from pokemon quiz

--- db/seeds/12_real_quiz.sql
+++ db/seeds/12_real_quiz.sql
@@ -10,20 +10,20 @@ INSERT INTO trivia_answers (question_id, answer) VALUES (62, 'Aerodactyl');
 INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (62, 'Lapras', true);
 INSERT INTO trivia_answers (question_id, answer) VALUES (62, 'Scyther');
 
-INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'Which of these Pokemon is NOT an electric type?');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (63, 'Porygon', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Voltorb');
-INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Zapdos');
-INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Plusle');
+-- INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'Which of these Pokemon is NOT an electric type?');
+-- INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (63, 'Porygon', true);
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Voltorb');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Zapdos');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (63, 'Plusle');
 
-INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'In Pokémon Go, which of these Pokémon can only be caught in Europe');
-INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Tauros');
-INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Farfetch''d');
-INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Kangaskhan');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (64, 'Mr. Mime', true);
+-- INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'In Pokémon Go, which of these Pokémon can only be caught in Europe');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Tauros');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Farfetch''d');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (64, 'Kangaskhan');
+-- INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (64, 'Mr. Mime', true);
 
-INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'Pokémon games usually begin with the user choosing from three ''starter Pokémon.'' Which of these is the correct trio of starters found in Pokémon Gold and Silver?');
-INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Bulbasaur, Charmander, Squirtle');
-INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Treecko, Torchic, Mudkip');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (65, 'Chikorita, Cyndaquil, Totodile', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Snivy, Tepig, Oshawott');
+-- INSERT INTO trivia_questions (quiz_id, question) VALUES (31, 'Pokémon games usually begin with the user choosing from three ''starter Pokémon.'' Which of these is the correct trio of starters found in Pokémon Gold and Silver?');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Bulbasaur, Charmander, Squirtle');
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Treecko, Torchic, Mudkip');
+-- INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (65, 'Chikorita, Cyndaquil, Totodile', true);
+-- INSERT INTO trivia_answers (question_id, answer) VALUES (65, 'Snivy, Tepig, Oshawott');
